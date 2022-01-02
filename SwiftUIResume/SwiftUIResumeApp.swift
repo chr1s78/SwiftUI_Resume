7dedb086d6e09532f34237b8049cadb5a8fe643c
chr1s78 <ayahirano2014@126.com>
2022-01-02 22:20:49 +0800

    Initial Commit

---
+++ SwiftUIResume/SwiftUIResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIResumeApp.swift
+//  SwiftUIResume
+//
+//  Created by Chr1s on 2022/1/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
